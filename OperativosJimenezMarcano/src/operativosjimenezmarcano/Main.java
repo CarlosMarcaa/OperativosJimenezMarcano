82d256e98b2c0b498536a2b8e5bd0228def9fd8b
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-01-28 15:08:56 -0400

    adds views in the graphical interface

--- OperativosJimenezMarcano/src/operativosjimenezmarcano/Main.java
+++ OperativosJimenezMarcano/src/operativosjimenezmarcano/Main.java
@@ -4,6 +4,8 @@
  */
 package operativosjimenezmarcano;
 
+import Interface.Views;
+
 /**
  *
  * @author Miguel
@@ -14,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Views views = new Views();
     }
     
 }
