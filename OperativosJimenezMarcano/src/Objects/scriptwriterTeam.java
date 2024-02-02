3b0d2557b36da05d19831d1b9145ffb33fc4ebe4
CarlosMarcaa <123030047+CarlosMarcaa@users.noreply.github.com>
2024-02-01 23:02:10 -0400

    adds scriptwriterTeam class 🎉

---
+++ OperativosJimenezMarcano/src/Objects/scriptwriterTeam.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objects;
+import static java.lang.Thread.sleep;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author cdmar
+ */
+public class scriptwriterTeam {
+ 
+    private Semaphore scriptwriterSemaphore;
+    private int employeeCount;
+    private int dayDuration;
+    private int dayCicle;
+    private int salaryAccount;
+    private drive scriptwriterDrive;
+    int salary = 20;
+    
+    public scriptwriterTeam(Semaphore scriptwriterSemaphore, int employeeCount, int dayDuration, drive scriptwriterDrive) {
+        this.scriptwriterSemaphore = scriptwriterSemaphore;
+        this.employeeCount = employeeCount;
+        this.dayDuration = dayDuration;
+        this.dayCicle = 0;
+        this.salaryAccount = 0;
+        this.scriptwriterDrive = scriptwriterDrive;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public int getDayDuration() {
+        return dayDuration;
+    }
+
+    public void setDayDuration(int dayDuration) {
+        this.dayDuration = dayDuration;
+    }
+
+    public int getDayCicle() {
+        return dayCicle;
+    }
+
+    public void setDayCicle(int dayCicle) {
+        this.dayCicle = dayCicle;
+    }
+
+    public int getSalaryAccount() {
+        return salaryAccount;
+    }
+
+    public void setSalaryAccount(int salaryAccount) {
+        this.salaryAccount = salaryAccount;
+    }
+    
+    public void addDailySalary(){
+        setSalaryAccount(
+                getSalaryAccount() + salary*24*getEmployeeCount()
+        );
+                            System.out.println("El equipo de "  + getEmployeeCount() + " guionistas" + " gana: " + salary*24*getEmployeeCount()+"$");
+    }
+
+    public drive getScriptwriterDrive() {
+        return scriptwriterDrive;
+    }
+    
+
+    public void run() {
+        setDayCicle(getDayCicle() + 1);
+        if (getDayCicle() >= 4) {
+            try {
+                getScriptwriterSemaphore().acquire(); //wait
+                int addedAmount = getScriptwriterDrive().add(getEmployeeCount()); //Adds 1 script for each employee in the team the function .add() in drive class returns the added amount to be reported later
+                System.out.println("El equipo de "  + getEmployeeCount() + " guionistas" + " agrego " + addedAmount + " guiones a su drive");
+                getScriptwriterSemaphore().release(); //wait
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(scriptwriterTeam.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public void operate() {
+ 
+            while (true){
+            
+                try {
+                    run();
+                    addDailySalary();
+                    sleep(this.dayDuration);                            
+                            
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(scriptwriterTeam.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+            }
+        
+    }
+
+    public Semaphore getScriptwriterSemaphore() {
+        return scriptwriterSemaphore;
+    }
+}
--- OperativosJimenezMarcano/src/Objects/studio.java
+++ OperativosJimenezMarcano/src/Objects/studio.java
@@ -2,22 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DataStructures;
+package Objects;
 
 /**
  *
  * @author cdmar
  */
 public class studio {
-    private queue employeeList;
-    
-    public queue getEmployeeList() {
-        return employeeList;
-    }
-
-    public void setEmployeeList(queue employeeList) {
-        this.employeeList = employeeList;
-    }
     
     
     
